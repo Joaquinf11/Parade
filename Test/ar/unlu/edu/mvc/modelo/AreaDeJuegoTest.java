ec8481642a4fc5f0d3d7d01fe94d4c5bd0488e21
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-10-26 10:30:10 -0300

    Update AreaDeJuegoTest.java

--- Test/ar/unlu/edu/mvc/modelo/AreaDeJuegoTest.java
+++ Test/ar/unlu/edu/mvc/modelo/AreaDeJuegoTest.java
@@ -12,28 +12,33 @@ class AreaDeJuegoTest {
     @BeforeEach
     void setUp(){
         area = new AreaDeJuego();
-    }
 
-    @Test
-    public void probandoSumarValorDeCartasDeAreaDeJuegoSinBocaAbajo(){
         area.agregarCarta(new Carta(0,Color.VERDE));
         area.agregarCarta(new Carta(0,Color.AMARILLO));
         area.agregarCarta(new Carta (5,Color.AZUL));
         area.agregarCarta(new Carta(8,Color.NEGRO));
         area.agregarCarta(new Carta(0,Color.ROJO));
         area.agregarCarta(new Carta(6,Color.VIOLETA));
+    }
+
+    @Test
+    public void probandogetCantidadDeCartasPorColor(){
+        int cantidad= area.getCantidadDeCartasPorColor(Color.VERDE);
+        assertEquals(1,cantidad);
+        area.sacarCarta(Color.VERDE,0);
+        cantidad=area.getCantidadDeCartasPorColor(Color.VERDE);
+        assertEquals(0,cantidad);
+
+    }
+
+    @Test
+    public void probandoSumarValorDeCartasDeAreaDeJuegoSinBocaAbajo(){
         int puntos=area.sumarValorDeCartas();
         assertEquals(-19,puntos);
     }
 
     @Test
     public void probandoSumarValorDeCartasDeAreaDeJuegoConBocaAbajo(){
-        area.agregarCarta(new Carta(0,Color.VERDE));
-        area.agregarCarta(new Carta(0,Color.AMARILLO));
-        area.agregarCarta(new Carta (5,Color.AZUL));
-        area.agregarCarta(new Carta(8,Color.NEGRO));
-        area.agregarCarta(new Carta(0,Color.ROJO));
-        area.agregarCarta(new Carta(6,Color.VIOLETA));
         area.ponerCartasBocaAbajo(Color.NEGRO);
         int puntos=area.sumarValorDeCartas();
         assertEquals(-11,puntos);
