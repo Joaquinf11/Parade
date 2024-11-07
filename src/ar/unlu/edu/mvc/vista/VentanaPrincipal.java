90978e04deab7c0071242974e8fd4ed5ec0a4e7b
Joaquin Santo Falco <104240501+Joaquinf11@users.noreply.github.com>
2024-11-06 21:18:52 -0300

    empezando VentanaPrincipal de la vista grafica

---
+++ src/ar/unlu/edu/mvc/vista/VentanaPrincipal.java
@@ -0,0 +1,16 @@
+package ar.unlu.edu.mvc.vista;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JPanel panelPrincipal;
+    private JLabel lTituloJuego;
+    private JPanel panelCentro;
+    private JButton agregarJugadorButton;
+    private JButton comenzarPartidaButton;
+    private JButton button3;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
