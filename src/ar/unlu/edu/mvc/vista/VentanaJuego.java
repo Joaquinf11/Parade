d9a2340c2ca225041d2659604e3c052383625368
Joaquin Santo Falco <104240501+Joaquinf11@users.noreply.github.com>
2024-11-06 23:32:42 -0300

    ventana juego

---
+++ src/ar/unlu/edu/mvc/vista/VentanaJuego.java
@@ -0,0 +1,13 @@
+package ar.unlu.edu.mvc.vista;
+
+import javax.swing.*;
+
+public class VentanaJuego {
+    private JPanel panelJuego;
+    private JPanel panelCartasEnMano;
+    private JButton carta0;
+    private JButton carta1;
+    private JButton carta2;
+    private JButton carta3;
+    private JButton carta4;
+}
