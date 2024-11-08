2ebe74d6f76cb874c92a600bd4d008ea04393532
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-07 22:40:46 -0300

    Cree IJuego

--- src/ar/unlu/edu/mvc/interfaces/IJuego.java
+++ src/ar/unlu/edu/mvc/interfaces/IJuego.java
@@ -1,5 +1,14 @@
 package ar.unlu.edu.mvc.interfaces;
 
+import java.util.List;
+
 public interface IJuego {
     IJugador getJugadorTurno();
+
+    void agregarJugador(String nombre);
+    List<IJugador> listarJugadores();
+
+    void empezarJuego();
+
+    void jugarCarta(int indice, int[] cartasElegidasCarnaval);
 }
