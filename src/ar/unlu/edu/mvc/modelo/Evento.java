4a0e2ab5904220cfd51d29fd4303ee7703fa59d5
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-07 23:54:26 -0300

    Update Evento.java

--- src/ar/unlu/edu/mvc/modelo/Evento.java
+++ src/ar/unlu/edu/mvc/modelo/Evento.java
@@ -3,6 +3,7 @@ package ar.unlu.edu.mvc.modelo;
 public enum Evento {
     JUGADOR_AGREGADO,
     CAMBIO_TURNO,
-    MAL_ELEGIDO_CARNAVAL,
-    CARTA_AGREGADA_CARNAVAL
+    NO_SE_PUEDE_AGARRAR,
+    CARTA_AGREGADA_CARNAVAL,
+    FIN_TURNO
 }
