167f9e1c66662d9d8326c33fe7f3edc7a4b845b7
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-08 19:38:03 -0300

    Ventana Ingresar Jugador

---
+++ src/ar/unlu/edu/mvc/vista/vistagrafica/VentanaIngresarJugador.java
@@ -0,0 +1,15 @@
+package ar.unlu.edu.mvc.vista.vistagrafica;
+
+import javax.swing.*;
+
+public class VentanaIngresarJugador {
+    private JPanel panelPrincipal;
+    private JLabel lTitulo;
+    private JTextField nombre;
+    private JPanel panelCentro;
+    private JButton aceptarButton;
+
+    public VentanaIngresarJugador(){
+
+    }
+}
