13e9fa3088b967de0f0f485ddfcde574cf354931
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-08 23:20:58 -0300

    Ventana menu inicial

---
+++ src/ar/unlu/edu/mvc/vista/vistagrafica/VentanaMenuInicial.java
@@ -0,0 +1,49 @@
+package ar.unlu.edu.mvc.vista.vistagrafica;
+
+import ar.unlu.edu.mvc.controlador.Controlador;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaMenuInicial {
+    protected JPanel panelPrincipal;
+    private JLabel lTitulo;
+    private JPanel panelCentro;
+    private JButton salirButton;
+    private JButton empezarButton;
+    private JButton agregarJugButton;
+
+    private VistaGrafica vistaGrafica;
+    private Controlador controlador;
+
+    public VentanaMenuInicial(Controlador controlador,VistaGrafica vistaGrafica){
+        this.controlador=controlador;
+        this.vistaGrafica=vistaGrafica;
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        agregarJugButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vistaGrafica.mostrarIngresarJugador();
+            }
+        });
+        empezarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //vistaGrafica.mostrarVentanaJuego();
+            }
+        });
+    }
+
+
+
+
+
+}
