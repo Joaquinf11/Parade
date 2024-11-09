735104033016a714de56671ea44cc24b5a37becf
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-08 23:21:32 -0300

    Ventan Ingresar Jugador

--- src/ar/unlu/edu/mvc/vista/vistagrafica/VentanaIngresarJugador.java
+++ src/ar/unlu/edu/mvc/vista/vistagrafica/VentanaIngresarJugador.java
@@ -1,15 +1,35 @@
 package ar.unlu.edu.mvc.vista.vistagrafica;
 
+import ar.unlu.edu.mvc.controlador.Controlador;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaIngresarJugador {
-    private JPanel panelPrincipal;
+    protected JPanel panelIngresarJugador;
     private JLabel lTitulo;
     private JTextField nombre;
     private JPanel panelCentro;
     private JButton aceptarButton;
 
-    public VentanaIngresarJugador(){
+    private Controlador controlador;
+    private VistaGrafica vistaGrafica;
 
+    public VentanaIngresarJugador(Controlador controlador,VistaGrafica vistaGrafica){
+        this.controlador=controlador;
+        this.vistaGrafica=vistaGrafica;
+        aceptarButton.addActionListener(new ActionListener() {
+
+
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controlador.agregarJugador(nombre.getText());
+                vistaGrafica.mostrarMenuInicial();
+            }
+
+        });
     }
+
 }
