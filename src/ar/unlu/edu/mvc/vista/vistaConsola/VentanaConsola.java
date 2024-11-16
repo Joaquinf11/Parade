5da39b7e6a36e83bcff438791a3061d0932328dc
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-15 21:32:41 -0300

    Update VentanaConsola.java

--- src/ar/unlu/edu/mvc/vista/vistaConsola/VentanaConsola.java
+++ src/ar/unlu/edu/mvc/vista/vistaConsola/VentanaConsola.java
@@ -5,7 +5,7 @@ import ar.unlu.edu.mvc.controlador.ControladorConsola;
 import javax.swing.*;
 import java.awt.*;
 
-public class VentanaConsola {
+public class VentanaConsola extends JFrame{
     private ControladorConsola controlador;
 
     private JPanel panelPrincipal;
@@ -13,17 +13,17 @@ public class VentanaConsola {
     private JPanel panelSalida; //TEDNRIA QUE SER UN BOX LAYOUR Y_AXIS
     private JTextField entradaField;
     private JTextArea salidaArea;
-    private final JFrame frame;
+
 
     public VentanaConsola (){
-        frame=new JFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setTitle("PARADE");
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 
-        frame.setContentPane(panelPrincipal);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("PARADE");
+        setExtendedState(Frame.MAXIMIZED_BOTH);
+
+        setContentPane(panelPrincipal);
         panelPrincipal.setLayout(new BoxLayout(panelPrincipal,BoxLayout.Y_AXIS));
         //panelSalida.setLayout(new BoxLayout(panelSalida,BoxLayout.Y_AXIS));
-        
+
     }
 }
