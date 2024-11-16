81606d7e9dd083babc6bff7a92fb7dbef7bda837
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-11-16 16:20:03 -0300

    Update Main.java

--- src/ar/unlu/edu/mvc/Main.java
+++ src/ar/unlu/edu/mvc/Main.java
@@ -1,8 +1,10 @@
 package ar.unlu.edu.mvc;
 
+import ar.unlu.edu.mvc.controlador.ControladorConsola;
 import ar.unlu.edu.mvc.controlador.ControladorGrafico;
 import ar.unlu.edu.mvc.modelo.Juego;
 import ar.unlu.edu.mvc.interfaces.IVista;
+import ar.unlu.edu.mvc.vista.vistaConsola.VentanaConsola;
 import ar.unlu.edu.mvc.vista.vistagrafica.paneles.VistaGrafica;
 
 public class Main {
@@ -12,25 +14,30 @@ public class Main {
         Juego juego = new Juego();
 
         IVista vista1= new VistaGrafica();
-        IVista vista2= new VistaGrafica();
-        IVista vista3= new VistaGrafica();
+       // IVista vista2= new VistaGrafica();
+      //  IVista vista3= new VistaGrafica();
         //IVista vista4= new VistaGrafica();
 
+        VentanaConsola consola= new VentanaConsola();
 
         ControladorGrafico controladorGrafico1 = new ControladorGrafico(juego,vista1);
-        ControladorGrafico controladorGrafico2 = new ControladorGrafico(juego,vista2);
-       ControladorGrafico controladorGrafico3 = new ControladorGrafico(juego,vista3);
+      //  ControladorGrafico controladorGrafico2 = new ControladorGrafico(juego,vista2);
+    //   ControladorGrafico controladorGrafico3 = new ControladorGrafico(juego,vista3);
      //   ControladorGrafico controladorGrafico4 = new ControladorGrafico(juego,vista4);
 
-        juego.agregarObservador(controladorGrafico1);
-        juego.agregarObservador(controladorGrafico2);
-       juego.agregarObservador(controladorGrafico3);
-       // juego.agregarObservador(controladorGrafico4);
+        ControladorConsola controladorConsola= new ControladorConsola(consola,juego);
 
+        juego.agregarObservador(controladorGrafico1);
+      //  juego.agregarObservador(controladorGrafico2);
+    //   juego.agregarObservador(controladorGrafico3);
+       // juego.agregarObservador(controladorGrafico4);
+        juego.agregarObservador(controladorConsola);
         controladorGrafico1.iniciar();
-        controladorGrafico2.iniciar();
-       controladorGrafico3.iniciar();
+      //  controladorGrafico2.iniciar();
+      // controladorGrafico3.iniciar();
      //   controladorGrafico4.iniciar();
+
+        controladorConsola.iniciar();
     }
 
 
