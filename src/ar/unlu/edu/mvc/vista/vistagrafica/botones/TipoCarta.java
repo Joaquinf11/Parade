4fb6f232ab1b7151f17686c02eba2c019d6e3ead
Joaquinf11 <joaquinfalco11@hotmail.com>
2024-12-02 13:20:03 -0300

    Create TipoCarta.java

---
+++ src/ar/unlu/edu/mvc/vista/vistagrafica/botones/TipoCarta.java
@@ -0,0 +1,17 @@
+package ar.unlu.edu.mvc.vista.vistagrafica.botones;
+
+public enum TipoCarta {
+    CARNAVAL,
+    MANO,
+    DORSO,
+    DORSO_VERTICAL,
+    NUM_AREA_VERTICAL,
+    ULTIMA_AREA_VERTICAL,
+    NUM_AREA_HORIZONTAL_IZQ,
+    NUM_AREA_HORIZONTAL_DER,
+    ULTIMA_AREA_HORIZONTAL_DER,
+    ULTIMA_AREA_HORIZONTAL_IZQ,
+    NUMERO_AREA,
+    ULTIMA_AREA
+
+}
