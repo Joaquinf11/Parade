ce5f2ecd43c8c8ba877ca398b61ee55cce542e4f
Joaquinf11 <joaquinfalco11@hotmail.com>
2025-01-18 16:32:40 -0300

    Update Jugador.java

--- src/ar/unlu/edu/mvc/modelo/Jugador.java
+++ src/ar/unlu/edu/mvc/modelo/Jugador.java
@@ -100,4 +100,12 @@ public class Jugador implements IJugador, Serializable {
     public int getVictorias(){
         return this.victorias;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Jugador jugador = (Jugador) o;
+        return nombre.equals(jugador.nombre);
+    }
 }
